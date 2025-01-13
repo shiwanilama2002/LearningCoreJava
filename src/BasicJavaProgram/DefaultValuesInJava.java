aa8a4a0499767e52a5ce559c877303faccad12cd
Shiwani Lama <162727035+shiwanilama2002@users.noreply.github.com>
2025-01-13 12:50:11 +0545

    Add files via upload

---
+++ src/BasicJavaProgram/DefaultValuesInJava.java
@@ -0,0 +1,20 @@
+package BasicJavaProgram;
+
+public class DefaultValuesInJava {
+	static int  num1;
+	static double d;
+	static char ch;
+	static String s;
+	static boolean b;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("The default value of integer datatype is:"+num1);
+		System.out.println("The default value of double datatype is"+d);
+		System.out.println("The default value of character datatype is:"+ch);
+		System.out.println("The default value of string datatype is:"+s);
+		System.out.println("The default value of boolean datatype is:"+b);
+		
+
+	}
+
+}
