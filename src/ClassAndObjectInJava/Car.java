c12e1cc395dce3731c9ee760bf43757254a5ec24
Shiwani Lama <162727035+shiwanilama2002@users.noreply.github.com>
2025-01-17 21:01:18 +0545

    Add files via upload

---
+++ src/ClassAndObjectInJava/Car.java
@@ -0,0 +1,18 @@
+package ClassAndObjectInJava;
+
+public class Car {
+	String brand="BMW";
+	String model="2024 Model"; 
+	String price="Rs.5crore";
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Car c = new Car();
+		System.out.println("The car is "+c.brand);
+		System.out.println("The model is "+c.model);
+		System.out.println("The price is "+c.price);
+		
+
+	}
+
+}
---
+++ src/ClassAndObjectInJava/Person.java
@@ -0,0 +1,21 @@
+package ClassAndObjectInJava;
+
+public class Person {
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Person p = new Person();
+		p.display();
+
+	}
+	public String display() {
+		String name ="Shiwani Lama";
+		int age = 22;
+		System.out.println(age);
+		return name;
+	
+		
+	}
+
+}
