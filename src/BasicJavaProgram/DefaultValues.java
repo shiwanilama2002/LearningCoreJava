361f3e637c5fa9233cc3af71bf3dd58061c73760
Shiwani Lama <162727035+shiwanilama2002@users.noreply.github.com>
2025-01-25 21:02:25 +0545

    Add files via upload

---
+++ src/BasicJavaProgram/DefaultValues.java
@@ -0,0 +1,19 @@
+package javalearning;
+
+public class DefaultValues {
+	static int num1;
+	static double num2;
+	static char c;
+	static boolean b;
+	static String s;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Printing the default values of data types");
+		System.out.println("The default value of int is:"+num1);
+		System.out.println("The default value of double is:"+num2);
+		System.out.println("The default value of char is:"+c);
+		System.out.println("The default value of boolean is:"+b);
+		System.out.println("The default value of string is:"+s);
+	}
+
+}
